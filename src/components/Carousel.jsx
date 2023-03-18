30d8cf22af57949a1d50b2c4a1a5c8136a1154b6
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-03-18 19:14:39 +0100

    agregando imagenes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,23 +7,23 @@ import img6 from '../assets/c6.png'
 
 function Carousel() {
   return (
-    <div className='h-[300px]'>
+    <div className=''>
     <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
       <div className="carousel-inner">
-        <div className="carousel-item active h-[250px]">
-          <img src={img1} className="d-block w-100 h-[250px]" alt="Imagen de muestra 1" />
+        <div className="carousel-item active">
+          <img src={img1} className="d-block w-100" alt="Imagen de muestra 1" />
         </div>
-        <div className="carousel-item h-[250px]">
-          <img src={img2} className="d-block w-100 h-[250px]" alt="Imagen de muestra 2" />
+        <div className="carousel-item">
+          <img src={img2} className="d-block w-100" alt="Imagen de muestra 2" />
         </div>
-        <div className="carousel-item h-[250px]">
-          <img src={img3} className="d-block w-100 h-[250px]" alt="Imagen de muestra 3" />
+        <div className="carousel-item">
+          <img src={img3} className="d-block w-100" alt="Imagen de muestra 3" />
         </div>
-        <div className="carousel-item h-[250px]">
-          <img src={img5} className="d-block w-100 h-[250px]" alt="Imagen de muestra 5" />
+        <div className="carousel-item">
+          <img src={img5} className="d-block w-100" alt="Imagen de muestra 5" />
         </div>
-        <div className="carousel-item h-[250px]">
-          <img src={img6} className="d-block w-100 h-[250px]" alt="Imagen de muestra 6" />
+        <div className="carousel-item">
+          <img src={img6} className="d-block w-100" alt="Imagen de muestra 6" />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
