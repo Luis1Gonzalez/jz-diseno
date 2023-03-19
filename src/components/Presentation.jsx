d9e9117038db42b781bf6729b652b7461a51f914
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-03-19 01:22:59 +0100

    agregando imagenes en index.jsx

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -2,12 +2,14 @@ import j1 from '../assets/j1.png'
 import j2 from '../assets/j2.png'
 import j3 from '../assets/j3.png'
 import j4 from '../assets/j4.png'
-import {RiSlideshowLine} from 'react-icons/ri'
-import {GiPencilRuler} from 'react-icons/gi'
-import {FaCalendarAlt} from 'react-icons/fa'
-import {FaRegHandshake} from 'react-icons/fa'
+import { RiSlideshowLine } from 'react-icons/ri'
+import { GiPencilRuler } from 'react-icons/gi'
+import { FaCalendarAlt } from 'react-icons/fa'
+import { FaRegHandshake } from 'react-icons/fa'
+import { offer } from '../data/objeto'
 
 export default function Presentation() {
+    console.log(offer)
     return (
         <div className="my-7 px-4">
 
@@ -20,50 +22,70 @@ export default function Presentation() {
             <div className='mb-20'>
                 <h2 className="text-center text-xl my-4">Nuestro Trabajo</h2>
 
-                <div className='h-[320px] pb-14 mb-3 rounded-lg border text-sm shadow-lg'>
-                    <img className='h-[100%] w-[100%] rounded-t-lg' src={j1} alt="imagen de reforma de cocina" />
-                    <p className='px-3 pt-3 '>Reforma de Cocinas</p>
-                </div>
-
-                <div className='h-[320px] pb-14 mb-3 rounded-lg border text-sm shadow-lg'>
-                    <img className='h-[100%] w-[100%] rounded-t-lg' src={j2} alt="imagen de instalación de parquet" />
-                    <p className='px-3 pt-3'>Instalación de Parquet</p>
-                </div>
-
-                <div className='h-[320px] pb-4 mb-3 rounded-lg border text-sm shadow-lg'>
-                    <img className='h-[90%] w-[100%] rounded-t-lg' src={j3} alt="imagen de reforma de baños" />
-                    <p className='px-3 pt-3 '>Reforma de Baños</p>
-                </div>
-
-                <div className='h-[320px] pb-4 mb-3 rounded-lg border text-sm shadow-lg'>
-                    <img className='h-[90%] w-[100%] rounded-t-lg' src={j4} alt="imagen de Fabricacón de muebles a medida" />
-                    <p className='px-3 pt-3 '>Fabricacón de muebles a medida</p>
+                <div className="my-7">
+                    {offer.map(of => (
+                        <div key={of.id} className='h-[320px] pb-14 mb-3 rounded-lg border text-sm shadow-lg'>
+                            <img className='h-[95%] w-[100%] rounded-t-lg' src={of.image} alt="imagen de reforma de cocina" />
+                            <p className='px-3 py-3 text-center'>{of.description}</p>
+                        </div>
+                    ))}
                 </div>
 
             </div>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <div className='flex flex-wrap justify-evenly'>
 
-<div className='w-[40%] mb-5 flex flex-col items-center'>
-    <span className='text-3xl mb-2 text-red-900'><RiSlideshowLine /></span>
-    <p className='text-xs text-center font-normal text-gray-500'>Asesoramiento profesional</p>
-</div>
+                <div className='w-[40%] mb-5 flex flex-col items-center'>
+                    <span className='text-3xl mb-2 text-red-900'><RiSlideshowLine /></span>
+                    <p className='text-xs text-center font-normal text-gray-500'>Asesoramiento profesional</p>
+                </div>
 
-<div className='w-[40%] mb-5 flex flex-col items-center'>
-    <span className='text-3xl mb-2 text-red-900'><GiPencilRuler /></span>
-    <p className='text-xs text-center font-normal text-gray-500'>Diseño personalizado</p>
-</div>
+                <div className='w-[40%] mb-5 flex flex-col items-center'>
+                    <span className='text-3xl mb-2 text-red-900'><GiPencilRuler /></span>
+                    <p className='text-xs text-center font-normal text-gray-500'>Diseño personalizado</p>
+                </div>
 
-<div className='w-[40%] mb-5 flex flex-col items-center'>
-    <span className='text-3xl mb-2 text-red-900'><FaCalendarAlt /></span>
-    <p className='text-xs text-center font-normal text-gray-500'>Proyecto sin compromisos</p>
-</div>
+                <div className='w-[40%] mb-5 flex flex-col items-center'>
+                    <span className='text-3xl mb-2 text-red-900'><FaCalendarAlt /></span>
+                    <p className='text-xs text-center font-normal text-gray-500'>Proyecto sin compromisos</p>
+                </div>
 
-<div className='w-[40%] mb-5 flex flex-col items-center'>
-    <span className='text-3xl mb-2 text-red-900'><FaRegHandshake /></span>
-    <p className='text-xs text-center font-normal text-gray-500'>Máxima garantía y cumplimiento de plazos</p>
-</div>
+                <div className='w-[40%] mb-5 flex flex-col items-center'>
+                    <span className='text-3xl mb-2 text-red-900'><FaRegHandshake /></span>
+                    <p className='text-xs text-center font-normal text-gray-500'>Máxima garantía y cumplimiento de plazos</p>
+                </div>
 
 
 
--- src/data/objeto.js
+++ src/data/objeto.js
@@ -1,16 +1,13 @@
-import carousel1 from '../assets/z2.jpg'
-import carousel2 from '../assets/z7.jpg'
-import carousel3 from '../assets/z8.jpg'
-import carousel4 from '../assets/z10.jpg'
-import carousel5 from '../assets/z12.jpg'
-import carousel6 from '../assets/z20.jpg'
+import j1 from '../assets/j1.png'
+import j2 from '../assets/j2.png'
+import j3 from '../assets/j3.png'
+import j4 from '../assets/j4.png'
 
-const carrusel = [
-    {id:1, name:'photo', image:carousel1},
-    {id:2, name:'photo', image:carousel2},
-    {id:3, name:'photo', image:carousel3},
-    {id:4, name:'photo', image:carousel4},
-    {id:5, name:'photo', image:carousel5},
-    {id:6, name:'photo', image:carousel6},
+
+const offer = [
+    {id:1, image: j1, description: 'Asesoramiento profesional'},
+    {id:2, image: j2, description: 'Diseño personalizado'},
+    {id:3, image: j3, description: 'Proyecto sin compromisos'},
+    {id:4, image: j4, description: 'Máxima garantía y cumplimiento de plazos'}
 ]
-export {carrusel}
+export {offer}
