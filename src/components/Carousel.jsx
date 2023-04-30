bc2cd2d08236d3aca8624c487ca077339ab1ab8c
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-04-30 21:23:02 +0200

    mejorando portada

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import imageGif from '../assets/imageGif.gif'
 function Carousel() {
   return (
 
-<div className='h-[400px] flex justify-center mt-4 bg-stone-100'>
+<div className='h-[200px] sm:h-[400px] flex justify-center mt-4 bg-stone-100'>
 <img className='h-[100%]' src={imageGif} alt='gif carrusel' />
 </div>
 
