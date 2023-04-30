44f135796040432bf125c1a52a1aef72e2fddc9c
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-04-30 21:16:16 +0200

    mejorando portada

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,39 +1,11 @@
-import img1 from '../assets/c1.png'
-import img2 from '../assets/c2.png'
-import img3 from '../assets/c3.png'
-import img5 from '../assets/c5.png'
-import img6 from '../assets/c6.png'
+import imageGif from '../assets/imageGif.gif'
 
 
 function Carousel() {
   return (
 
-<div id="carouselExampleAutoplaying" className="carousel slide flex justify-center" data-bs-ride="carousel">
-  <div className="carousel-inner  sm:w-[75%] otro opacity-80 sm:rounded-lg sm:border sm:shadow-lg">
-    <div className="carousel-item active ">
-      <img src={img1} className="d-block w-100 rounded-2xl" alt="Imagen de muestra 1" />
-    </div>
-    <div className="carousel-item">
-      <img src={img2} className="d-block w-100 rounded-2xl" alt="Imagen de muestra 2" />
-    </div>
-    <div className="carousel-item">
-      <img src={img3} className="d-block w-100 rounded-2xl" alt="Imagen de muestra 3" />
-    </div>
-    <div className="carousel-item">
-      <img src={img5} className="d-block w-100 rounded-2xl" alt="Imagen de muestra 4" />
-    </div>
-    <div className="carousel-item">
-      <img src={img6} className="d-block w-100 rounded-2xl" alt="Imagen de muestra 5" />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
+<div className='h-[400px] flex justify-center mt-4 bg-stone-100'>
+<img className='h-[100%]' src={imageGif} alt='gif carrusel' />
 </div>
 
 );
